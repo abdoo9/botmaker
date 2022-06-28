62ae394a246e1e0a0e319aab4d0c058ca3e33fec
abdo <abdulrahman100b@gmail.com>
2022-06-28 13:35:24 +0300

    testing the costumwebhook example

--- venv/app.py
+++ venv/app.py
@@ -13,25 +13,23 @@ app = Flask(__name__)
 
 application = Application.builder().token(TOKEN).build()
 
-def start(update, context):
-    update.message.reply_text("Hello, I am a bot")
 
-    
-application.add_handler(CommandHandler('start', start))
+async def start(update, context):
+    await update.message.reply_text("Hello, I am a bot")
 
 
 @app.route('/', defaults={'path': ''}, methods=['GET', 'POST'])
 @app.route('/<path:path>', methods=['GET', 'POST'])
-def respond(path):
-    path = path.replace('%', ':')
-    print(path)
+async def respond(path):
     # retrieve the message in JSON and then transform it to Telegram object
     update = request.get_json()
     print(update)
-    Application.process_update(self=application, update=update)
-    
+    await Application.initialize(application)
+    await Application.process_update(self=application, update=update)
+
     return 'ok'
 
-
 if __name__ == '__main__':
-       app.run(port=8443, ssl_context=('C:\\Users\\abdoN\\botmaker\\venv\\cert.pem', r'C:\\Users\\abdoN\\botmaker\\venv\\private.key'))
+    application.add_handler(CommandHandler('start', start))
+    Application.start(application)
+    app.run(port=8443, ssl_context=('C:\\Users\\abdoN\\botmaker\\venv\\cert.pem', r'C:\\Users\\abdoN\\botmaker\\venv\\private.key'))
